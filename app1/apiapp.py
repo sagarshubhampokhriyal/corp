b06af3d684287508cb9f37f1bb946f8a0cf96f46
sagarshubhampokhriyal <120398676+sagarshubhampokhriyal@users.noreply.github.com>
2023-07-12 20:46:57 +0530

    Update apiapp.py

--- app1/apiapp.py
+++ app1/apiapp.py
@@ -1,27 +1,11 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras 
-from tensorflow.keras import Sequential
 import pickle
-from tensorflow.keras.layers import Dense,Conv2D,MaxPool2D,Flatten,BatchNormalization,Dropout,Input
 from sklearn.tree import DecisionTreeClassifier
 
 dt = pickle.load(open('dt.sav', 'rb'))
 lg = pickle.load(open('lg.sav', 'rb'))
 svm = pickle.load(open('svm.sav', 'rb'))
 knn = pickle.load(open('knn.sav', 'rb'))
-
-model=Sequential()
-model.add(Input(shape=(7)))
-model.add(Dense(73,activation='relu'))
-model.add(BatchNormalization(axis=1))
-model.add(Dense(147,activation='relu'))
-model.add(BatchNormalization(axis=1))
-model.add(Dense(37,activation='relu'))
-model.add(BatchNormalization(axis=1))
-model.add(Dense(22,activation='softmax'))
-
-model.load_weights('nn.h5')
 
 
 from flask import Flask,request,jsonify,Blueprint, redirect, url_for
@@ -56,8 +40,6 @@ def predict():
 
 	LG_predict=lg.predict(query)
 
-	y_prob=model.predict(query)
-	NN_predict=y_prob.argmax(axis=1)
 
 	vote=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	maxVote=0
@@ -72,10 +54,6 @@ def predict():
 	if maxVote<vote[SVM_predict[doc]]:
 	    label=SVM_predict[doc]
 	    maxVote=vote[SVM_predict[doc]]
-	vote[NN_predict[doc]]+=1
-	if maxVote<vote[NN_predict[doc]]:
-	    label=NN_predict[doc]
-	    maxVote=vote[NN_predict[doc]]
 	vote[LG_predict[doc]]+=1
 	if maxVote<vote[LG_predict[doc]]:
 	    label=LG_predict[doc]
